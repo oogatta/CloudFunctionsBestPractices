a75c7604dd538b35107d12e075a7a0f27aac4ffd
oogatta <oogatta@gmail.com>
2018-10-28 23:49:57 +0900

    http関数のテスト

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -1,8 +1,25 @@
-import { test } from "../src/index";
+import * as FunctionsTest from "firebase-functions-test";
+import { test, helloWorld } from "../src";
 import { expect } from "chai";
 import "mocha";
+import {Request, Response} from "express";
 
-describe("test functions", () => {
+FunctionsTest().mockConfig({});
+
+describe("helloWorld function", () => {
+  it("test", () => {
+    const request = {};
+    const response = {
+      send: (body) => {
+        expect(body).to.equal("Hello from Firebase!");
+        return this;
+      }
+    };
+    helloWorld(request as Request, response as Response);
+  });
+});
+
+describe("test function", () => {
   it("should return 1.", () => {
     expect(test()).to.equal(1);
   });
