90fd219ca07133720c08c8465559dcd9f2adb745
oogatta <oogatta@gmail.com>
2018-10-31 15:42:36 +0900

    firestoreをモックできた

--- functions/test/functions/observeCreateUser.test.ts
+++ functions/test/functions/observeCreateUser.test.ts
@@ -1,9 +1,12 @@
-import { expect } from "chai";
 import "mocha";
+import {expect} from "chai";
+import sinon = require("sinon");
 
 import * as FunctionsTest from "firebase-functions-test";
 const test = FunctionsTest();
 test.mockConfig({});
+
+import * as admin from "firebase-admin";
 
 import observeCreateUser from "../../typescript/functions/observeCreateUser";
 
@@ -14,12 +17,45 @@ describe("observeCreateUser function", () => {
 
   it("should return 1.", () => {
     const documentSnapshot = test.firestore.makeDocumentSnapshot({ name: "Naohiro Oogatta" }, "users/oogatta");
-    const wrappedOserveCreateUser = test.wrap(observeCreateUser);
+    const wrappedObserveCreateUser = test.wrap(observeCreateUser);
 
-    wrappedOserveCreateUser(documentSnapshot, {
+    const firestoreObject = new Firestore();
+    sinon.stub(admin, "firestore")
+      .get(() => () => firestoreObject);
+
+    const testCollectionObject = new CollectionReference();
+    sinon.stub(firestoreObject, "collection")
+      .withArgs("test")
+      .returns(testCollectionObject);
+
+    const userIdDocObject = new DocumentReference();
+    sinon.stub(testCollectionObject, "doc")
+      .returns(userIdDocObject);
+
+    const set = sinon.stub(userIdDocObject, "set");
+
+    wrappedObserveCreateUser(documentSnapshot, {
       params: {
         userId: "oogatta"
       }
     });
   });
 });
+
+class Firestore {
+  collection(collectionpath: string): CollectionReference {
+    return new CollectionReference();
+  }
+}
+
+class CollectionReference {
+  doc(documentPath: string): DocumentReference {
+    return new DocumentReference();
+  }
+}
+
+class DocumentReference {
+  set(documentRef: DocumentReference, data: any, options?: any): any {
+    return 1;
+  }
+}
--- functions/typescript/functions/observeCreateUser.ts
+++ functions/typescript/functions/observeCreateUser.ts
@@ -2,14 +2,18 @@ import * as functions from "firebase-functions";
 
 import * as admin from "firebase-admin";
 admin.initializeApp();
+admin.firestore().settings({ timestampsInSnapshots: true });
 
 export default functions
   .region("asia-northeast1")
   .firestore
   .document("users/{userId}")
   .onCreate((documentSnapshot, context) => {
-    console.log(context.params.userId);
-    console.log(documentSnapshot.data());
+    // console.log(context.params.userId);
+    // console.log(documentSnapshot.data());
 
-    return 1;
+    return admin.firestore()
+      .collection("test")
+      .doc(context.params.userId)
+      .set({test: true});
   });
