e70f8d93298783bfbe342e48ef81dfdc5e576c44
oogatta <oogatta@gmail.com>
2018-10-31 16:43:12 +0900

    TODO一つ完了

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@
 const exportIfNeeded = require("./lib/util/exportIfNeeded").default;
 
 // TODO: Realtime Database トリガーの関数とテスト
-// TODO: Firestore トリガーのテスト
 // TODO: Storage トリガーの関数とテスト
 
 exportIfNeeded("helloWorld", exports);
